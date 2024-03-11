31d17c2bdc70f0c3a30c0fc903cb4691a8b254d8
phiraphonnn <65663938+phiraphonnn@users.noreply.github.com>
2024-03-12 05:08:45 +0700

    animtor

--- TerraForge/Assets/UnitBehevior.cs
+++ TerraForge/Assets/UnitBehevior.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Unity.Netcode;
+using Unity.Netcode.Components;
 
 public class UnitBehevior : NetworkBehaviour
 {
@@ -9,9 +10,9 @@ public class UnitBehevior : NetworkBehaviour
     [SerializeField] private Rigidbody2D rb;
     [SerializeField] private float movementSpeed = 4f;
     private Vector2 targetPosition;
-
+    public SpriteRenderer SpriteRenderer;
     public Animator Animator;
-    
+    public NetworkAnimator NetworkAnimator;
     private void Update()
     {
         if (!IsOwner) { return; }
@@ -25,18 +26,29 @@ public class UnitBehevior : NetworkBehaviour
     private void FixedUpdate()
     {
        if (!IsOwner) { return; }
-      
-        
-        if (Vector2.Distance(rb.position, targetPosition) > 0.1f)
+
+
+       if (Vector2.Distance(rb.position, targetPosition) > 0.1f)
+       {
+         //  Animator.SetBool("IsWalk", true);
+          FlipXWalkServerRpc(true);
+           Vector2 moveDirection = (targetPosition - (Vector2) rb.position).normalized;
+           rb.velocity = moveDirection * movementSpeed;
+           if (moveDirection.x > 0) // Moving right
+               SpriteRenderer.flipX = false;
+           else if (moveDirection.x < 0) // Moving left
+               SpriteRenderer.flipX = true;
+       }
+       else
         {
-            Animator.SetBool("IsWalk", true);
-            Vector2 moveDirection = (targetPosition - (Vector2)rb.position).normalized;
-            rb.velocity = moveDirection * movementSpeed;
-        }
-        else
-        {
-            Animator.SetBool("IsWalk", false);
+            FlipXWalkServerRpc(false);
             rb.velocity = Vector2.zero;
         }
+    }
+
+    [ServerRpc]
+    private void FlipXWalkServerRpc(bool IsWalk)
+    {
+        NetworkAnimator.Animator.SetBool("IsWalk", IsWalk);
     }
 }
